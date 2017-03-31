fe30b190bbebb02f1cfce8d7130cdc15754c56e3
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-03-31 12:46:10 -0500

    [add] custom document

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Document, {
+  Main,
+  NextScript,
+  Head,
+} from 'next/document';
+
+class PlatziMusic extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>PlatziMusic</title>
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default PlatziMusic;
