96916bf9f2b997d9181a35324332cc34d51befe6
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-03-31 18:56:08 -0500

    [update] load data from api

--- pages/results.js
+++ pages/results.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import fetch from 'isomorphic-fetch'
 
+class ResultsPage extends Component {
+  static async getInitialProps({ query }) {
+    const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist`
 
-function ResultsPage() {
-  return (
-    <div>
-      <h1>Resultados</h1>
-      <Link href="/">
-        <a>Ir a la home</a>
-      </Link>
-    </div>
-  );
+    const response = await fetch(URL)
+
+    const data = await response.json()
+
+    return data
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <ul>
+        {
+          this.props.artists.items.map(
+            item => (
+              <li key={item.id}> {item.name} </li>
+            )
+          )
+        }
+      </ul>
+    )
+  }
 }
 
 export default ResultsPage;
