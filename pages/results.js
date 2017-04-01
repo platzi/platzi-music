d26306906b2ff0c559be7615f4256dc3e1350fe9
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-03-31 20:13:06 -0500

    [update] tracks and albums

--- pages/results.js
+++ pages/results.js
@@ -4,27 +4,49 @@ import fetch from 'isomorphic-fetch'
 
 class ResultsPage extends Component {
   static async getInitialProps({ query }) {
-    const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist`
+    const URL = `https://api.spotify.com/v1/search/?q=${query.query}&type=artist,album,track`
 
     const response = await fetch(URL)
 
     const data = await response.json()
-
+    console.log(data);
     return data
   }
 
   render() {
     console.log(this.props)
     return (
-      <ul>
-        {
-          this.props.artists.items.map(
-            item => (
-              <li key={item.id}> {item.name} </li>
+      <div>
+        <ul>
+          {
+            this.props.tracks.items.map(
+              item => (
+                <li key={item.id}> {item.name} </li>
+              )
             )
-          )
-        }
-      </ul>
+          }
+        </ul>
+        <ul>
+          {
+            this.props.albums.items.map(
+              item => (
+                <li key={item.id}> {item.name} </li>
+              )
+            )
+          }
+        </ul>
+        <ul>
+          {
+            this.props.artists.items.map(
+              item => (
+                <li key={item.id}> {item.name} </li>
+              )
+            )
+          }
+        </ul>
+
+
+      </div>
     )
   }
 }
