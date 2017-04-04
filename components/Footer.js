0835a4702f97410037aaeedb4705534d71b718ff
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2017-04-04 14:12:41 -0500

    [update] take the Footer json from a diferent file

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,9 @@
 import React from 'react'
 import { Row, Col } from 'react-styled-flexboxgrid'
 import FooterItem from './FooterItem'
+import items from '../static/content/footer.json'
 
 function Footer() {
-  const items = {
-    item1: {
-      title: 'Mauris acv arius arcu',
-      text: 'Proin dolor libero, interdum vitae lorem et, pellentesque feugait urna. Nulla viverra, ex quis interdum malesuada, tellus enim viverra nulla'
-    },
-    item2: {
-      title: 'Nulla viverra',
-      text: 'Dolor libero, interdum vitae lorem et, pallentesque feugiat urna. Nulla viverra, ex'
-    },
-    item3: {
-      title: 'Usce ornare',
-      text: 'Ain dolor libero, interdum vitae lorem et, pallentesque feugiat urna. Nulla uis interdum malesu tellus enim viverra nulla'
-    }
-  }
   return (
     <Row center="xs">
       <Col xs={12} sm={4}>
